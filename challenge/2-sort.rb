402d23042afeac08b391ca3596d9c29474db5a2e
ShAlabbadi <10708@holbertonstudents.com>
2025-09-26 16:37:07 +0000

    2. Sort

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -8,9 +8,8 @@ result = []
 ARGV.each do |arg|
     # skip if not integer
     if arg !~ /^-?[0-9]+$/
-
-    # convert to integer
-    i_arg = arg.to_i
+      # convert to integer
+      i_arg = arg.to_i
     
     # insert result at the right position
     is_inserted = false
